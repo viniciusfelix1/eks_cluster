20f06685720148acf961b5cab5881ac79087e68a
viniciusfelix1 <vinicius.felix.1@hotmail.com>
2021-02-07 23:30:16 -0300

    Adicionado váriaveis para o RDS e para o ALB

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,7 +2,9 @@ variable "AWS_REGION" {
     default = "us-east-1"
 }
 
+#
 ## VPC
+#
 variable "cidr_block" {
   description = "Rede principal utilizada pela VPC."
   default = "192.168.0.0/16"
@@ -23,7 +25,9 @@ variable "qtd_eip"{
   default = "2"
 } 
 
+#
 ## EKS
+#
 variable "cluster_name" {
   description = "O nome que será dado ao cluster."
   default = "eks_cluster"
@@ -39,3 +43,37 @@ variable "node_group_instances" {
   default = ["t3.micro"]
 }
 
+#
+## RDS
+#
+
+variable "rds_name" {
+  default = "mediawikirds"
+  description = "Identificados do cluster"
+}
+
+variable "rds_instance_class" {
+  default = "db.t2.micro"
+  description = "Tipo de instancia a ser utilizada pelo RDS"
+}
+
+variable "rds_engine" {
+  default = "mariadb"
+  description = "RDS Engine used"
+}
+
+variable "rds_engine_version" {
+  default = "10.5"
+  description = "RDS Engine version to be used"
+}
+
+variable "rds_user" {
+  type = string
+  description = "Usuário de acesso ao RDS. Conteúdo buscado em variável de ambiente."
+  sensitive = true
+}
+variable "rds_pass" {
+  type = string
+  description = "Senha de acesso ao RDS. Conteúdo buscando em variável de ambiente."
+  sensitive = true
+}
