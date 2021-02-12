da6b01a1e87af367e3e21cb0b335198812ab060b
Vinícius <vinicius.felix.1@hotmail.com>
2021-02-12 22:53:15 +0000

    Adicionado dockerfile para build da imagem de restore do mariadb

---
+++ dockerfiles/dump/script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#MYSQL_PW=$(echo "${MYSQL_PW}" | base64 -d)
+
+echo "${MYSQL_PW} ${MYSQL_HOST} ${MYSQL_USER} ${MYSQL_NAME}"
+
+if [ ! -z "$MYSQL_PW"  ]; then
+   mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PW} ${MYSQL_NAME} < backup.sql
+ else
+   echo "NÃo foi identificado senha, fazendo a conexÃo sem senha."
+   mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} ${MYSQL_NAME} < backup.sql
+fi
+
+if [ $? -eq 0 ]; then
+   echo "Restore realizado com exito."
+ else
+   echo "Falha ao restaurar o backup."
+fi
